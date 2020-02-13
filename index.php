e923d2c6e2772d47b5eb0f1e792582678f8acca0
viotalJiplk <61029138+viotalJiplk@users.noreply.github.com>
2020-02-13 22:18:06 +0100

    0.0.2b
    upload verze 0.0.2b

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>SPGT-Studentský parlament Gymnázia Tišnov</title>
+		<link rel="stylesheet" href="css/main_page.css">
+	</head>
+	<body>
+		<?php require 'include/nav.html';?>
+		<div class="titleBox">
+			<div class="titleBoxText">
+				<h1>Studenti pro studenty</h1>
+				<h2>Zapojte se... bla bla bla</h2>
+			</div>
+		</div>
+		<div class="articleBox">
+			<?php require 'articles/2019-2020/2020.1.26 testovaci.html';?>
+		</div>
+		<div class="meetingsBox">
+			<h3>Zapojte se přímo na schůzích, nebo nás kontaktujte.</h3>
+		</div>
+	</body>
+</html>
---
+++ zasedani.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Zápisy</title>
+	</head>
+	<body>
+		<?php require 'include/nav.html';?>
+		<h1></h1>
+	</body>
+</html>
