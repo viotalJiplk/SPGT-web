5f90992619bd383f43ed91457af89e184e112590
Vojta Vařecha <vojta@varechovi.eu>
2020-02-17 11:58:14 +0100

    added login form

--- login.php
+++ login.php
@@ -3,4 +3,11 @@
     <head>
         <meta charset="UTF-8">
     </head>
+    <body>
+        <form action="login.php" method="post">
+            <input type="text" value="username" name="username" required/>
+            <input type="password" name="password" required/>
+            <button type="submit">Login</button>
+        </form>
+    </body>
 </html>
