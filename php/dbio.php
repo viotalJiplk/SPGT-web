819dcd702a030005d929a156db8a0faba4fd99d2
viotalJiplk <viotal.varecha@outlook.com>
2020-02-19 09:17:29 +0100

    adding dbio.php to use in database i/o

---
+++ php/dbio.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once("classdef.php");
+
+function dbio(string $sql, int $io){
+    $servername = "localhost";
+    $dbname = "rohlinekgame";
+
+    $username = "rohlinekgame";
+    $password = "123";
+
+
+    $conn = new PDO("mysql:host = $servername; dbname = $dbname", $username, $password);
+
+            //adding parmeters and testing return value
+    if(!$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)){    //PDO::ERRMODE_SILENT for not debug uses
+        throw new AttrException("Failed to set atribute (ERRMODE)");
+    }
+    if(!$conn->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING)){    //set atribute allways 2 param, empety = NULL
+        throw new AttrException("Failed to set atribute (NULL_EMPTY_STRING)");
+    }
+    if(!$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC)){ //array only with collum name (not indexed ones)
+        throw new AttrException("Failed to set atribute (FETCH_MODE)");
+    }
+        
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    if($io){
+        $result = $stmt -> fetchAll();    
+        return $result;
+        $conn = null;
+    }
+}
+
+?>
