a37e0763a98681107e841536b6ac5aebcca73917
NemoCZ <niktonicnic@gmail.com>
2020-03-01 11:04:30 +0100

    BrowserSync setup.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
 
+const sass = require('gulp-sass');
 sass.compiler = require('sass');
 
 const browserSync = require('browser-sync').create();
@@ -13,8 +13,20 @@ function compile(){
     // compile them
     .pipe(sass())
     // put css into its folder
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
 
+    .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        proxy: "spgt"
+    });
+
+    gulp.watch('./scss/**/*.scss', compile);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+    gulp.watch('./*.php').on('change', browserSync.reload);
 }
 
 exports.scsscomp = compile;
+exports.watch = watch;
