e86b2a94637d6c12bbf059d6ce340ff7e885fe5e
NemoCZ <niktonicnic@gmail.com>
2020-03-14 13:03:30 +0100

    Added articles.php

---
+++ articles.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>SPGT-Studentský parlament Gymnázia Tišnov</title>
+		<link rel="stylesheet" href="css/main_page.css">
+	</head>
+	<body>
+		<?php require 'include/nav.html';?>
+		<div class="main">
+			
+		</div>
+	</body>
+</html>
