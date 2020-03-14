3036887b6c387de92e989feae727cdeb9082d918
NemoCZ <niktonicnic@gmail.com>
2020-03-14 13:06:12 +0100

    Added articles.scss

--- articles.php
+++ articles.php
@@ -3,12 +3,12 @@
 	<head>
 		<meta charset="UTF-8"/>
 		<title>SPGT-Studentský parlament Gymnázia Tišnov</title>
-		<link rel="stylesheet" href="css/main_page.css">
+		<link rel="stylesheet" href="css/articles.css">
 	</head>
 	<body>
 		<?php require 'include/nav.html';?>
 		<div class="main">
-			
+
 		</div>
 	</body>
 </html>
