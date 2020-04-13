515060ea5cd1453a179cb6b17a2b341e48d80662
Vojta Vařecha <vojta@varechovi.eu>
2020-04-13 13:10:55 +0200

    add get all id, time: (zapisygetall.php)

---
+++ endpoints/zapisygetall.php
@@ -0,0 +1,13 @@
+<?php
+    include(dirname(__DIR__)."/php/dbio.php");                     //the location will be found even when this file (zapisy.php) was included
+    include(dirname(__DIR__)."/php/classdef.php");
+    header("Content-type: application/json");
+
+    try{
+        $result = dbio("SELECT id,time FROM d215865_spgtweb.zapisy",1);
+        echo json_encode($result);
+    }catch(dbIOException $e){                                               //some db exception
+    $error = "system exception";
+    echo json_encode($e);
+    }
+?>
