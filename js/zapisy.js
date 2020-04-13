df6322b8ee1fe88010f04db3e2868eb2df1eb7ce
Vojta Vařecha <vojta@varechovi.eu>
2020-04-13 12:31:08 +0200

    addad dochazka

--- js/zapisy.js
+++ js/zapisy.js
@@ -26,10 +26,11 @@ function callbackf(text){
     var obj = document.getElementById(json[0].id).getElementsByClassName("zasedaniCont")[0];
     var result = json[0];
     var hlasovani = result.hlasovani;
-    var dochazka = "";
+    var dochazkaZ = result.dochazkaZ;
+    var dochazkaD = result.dochazkaD;
     var program = result.program;
     var materialy = result.materialy;
-    var zasedaniCont = "<div class=\"program\"><h4>Program Zasedání</h4>" + program + "</div><div class=\"downloads\"><h4>Ke stažení</h4><button class=\"zapis\">Stáhnout zápis</button><div class=\"materialy\"><h5>Materiály</h5>" + materialy +"</div></div><div class=\"statistiky\"><div class=\"dochazka\"><h4>Docházka</h4>" + dochazka + "</div><div class=\"hlasovani\"><h4>Hlasování</h4>" + hlasovani + "</div></div>";
+    var zasedaniCont = "<div class=\"program\"><h4>Program Zasedání</h4>" + program + "</div><div class=\"downloads\"><h4>Ke stažení</h4><button class=\"zapis\">Stáhnout zápis</button><div class=\"materialy\"><h5>Materiály</h5>" + materialy +"</div></div><div class=\"statistiky\"><div class=\"dochazka\"><h4>Docházka</h4><p>Zástupci tříd: " + dochazkaZ + "</p><p>Dobrovolníci: " + dochazkaD +"</p></div><div class=\"hlasovani\"><h4>Hlasování</h4>" + hlasovani + "</div></div>";
     console.log(zasedaniCont);
     obj.innerHTML = zasedaniCont;
     
