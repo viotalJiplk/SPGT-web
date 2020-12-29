63a11895402e90176cc00145323a46cfd1b2c17c
NemoCZmoravia <niktonicnic@gmail.com>
2020-12-29 23:51:56 +0100

    Working on dark mode, saving progress...

--- testing.php
+++ testing.php
@@ -3,12 +3,24 @@
 	<head>
 		<meta charset="UTF-8"/>
 		<title>SPGT-Studentský parlament Gymnázia Tišnov</title>
-		<link rel="stylesheet" href="css/main_page.css">
+		<link rel="stylesheet" href="css/testing.css">
 		<link rel="shortcut icon" href="favicon.svg" type="image/svg+xml">
 	</head>
 	<body>
 		<div class="card elevate1">
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis labore fuga cupiditate. Explicabo, amet voluptatem recusandae eligendi optio quasi provident deleniti suscipit repellat illo, qui dolorem in soluta dolor iusto.</p>
+			<p class="card elevate1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis labore fuga cupiditate. Explicabo, amet voluptatem recusandae eligendi optio quasi provident deleniti suscipit repellat illo, qui dolorem in soluta dolor iusto.</p>
+		</div>
+
+		<div id="box" class="">
+			<p>test</p>
+		</div>
+
+		<div id="box" class="elevation1">
+			<p>test</p>
+		</div>
+
+		<div id="box" class="elevation2">
+			<p>test</p>
 		</div>
 	</body>
 </html>
