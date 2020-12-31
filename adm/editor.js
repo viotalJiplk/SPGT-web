f4f1b8787e9a21850e380023055e25da2841ea86
Vojtech Varecha <viotal.varecha@outlook.com>
2020-12-31 20:55:10 +0100

    spgt editor 0.0.1

--- adm/editor.js
+++ adm/editor.js
@@ -1,102 +1,164 @@
-var rangearray;
-function getselection(funct, on){
+document.execCommand('defaultParagraphSeparator', false, 'p');
+const tags = {
+    "b": {
+        tagname: "bolt",
+        type: "inline",
+        in_tags: ["p"],
+        replace_tags:[],
+    },
+    "h1":{
+        tag: "heading",
+        type: "newline",
+        in_tags: [],
+        replace_tags:["p"],
+    }
+};
+
+/**
+ * function which get curent selection(s), make documentFragment(s) from it (them) and passes it (them) to edit function and after processing from edit() put documentFragment(s) back to document
+ * @param {string} tag - name of tag from tags object (h1, b...). (would be passed to edit())
+ * @param {boolean} on - 0 = undotag, 0 != add tag (would be passed to edit())
+ */
+function getselection(tag, on){
     var selection = window.getSelection();
 
     for(i = 0; i< selection.rangeCount; i++){
-        var firstnode;
-        var endnode;
         var range = selection.getRangeAt(i);
 
         firstnode = range.startContainer;
         endnode = range.endContainer;
-        console.log(firstnode);
-        console.log(endnode);
-        console.log();
 
         documentFragment = range.extractContents();
         /*
         code to manipulate selected elements (in documentFragment)
         */
-        if (funct == "heading"){
-            heading(documentFragment, on);
-        }else if(funct == "bolt"){
-            bolt(documentFragment, on);
-        }
+        edit(documentFragment, tag, on);
+
         
         /*
         end of code to manipulate selected elements (in documentFragment)
         */
         //last node first
-        console.log(firstnode);
-        console.log(endnode);
         while(0 < documentFragment.childNodes.length){
             range.insertNode(documentFragment.childNodes[documentFragment.childNodes.length - 1])                 
         }
-        document.getElementsByClassName("contenteditable")[0].innerHTML = document.getElementsByClassName("contenteditable")[0].innerHTML.replaceAll(/(<[^<,/]*>)(<[^<]*>)*\s*(<\/[^<]*>)(<\/[^<]*>)*/gm, ""); //this should delete all empety tags
+        range.detach();
+        //document.getElementsByClassName("contenteditable")[0].innerHTML = document.getElementsByClassName("contenteditable")[0].innerHTML.replaceAll(/(<[^<,/]*>)(<[^<]*>)*\s*(<\/[^<]*>)(<\/[^<]*>)*/gm, ""); //this should delete all empety tags
     }
 }
 
-function replacetag(html, tags, replace, check_rep){
-    if(tags.length > replace.length){
-        throw "not enought replace tags (replace tag function)"
-    }
-    for(i2 = 0; i2 < tags.length; i2++){
-        var rep_openingtag = "";
-        var rep_closingtag = "";
+/**
+ * main edit function decides if tag is inline or newline and calls specific function to do the "dirty job" 
+ * @param {Node} documentFragment - part of the document to edit.
+ * @param {string} tag - name of tag from tags object (h1, b...).
+ * @param {boolean} state - 0 = undotag, 0 != add tag
+ */
+function edit(documentFragment, tag, state){
+    tags[tag].replace_tags.forEach(function(replace_tag){
+        if(state != 0){
+            var tag_to_replace_selector = replace_tag;
+            var replacement = tag;
 
-        if (!(replace[i2] == "")){
-            rep_openingtag = "<" + replace[i2] + ">";
-            rep_closingtag = rep_openingtag.replaceAll(/</g, "</");
-        }
-        if(check_rep == 0){
-            var openingtag = new RegExp("<" + tags[i2] + ">", "g");
-            var closingtag = new RegExp("</" + tags[i2] + ">", "g");
+            //firstandlastnodeon(documentFragment, tag);
         }else{
-            var openingtag = new RegExp("<" + tags[i2] + ">(?!" + "<" + replace[i2] + ">)", "g");
-            var closingtag = new RegExp("</" + tags[i2] + ">(?!" + "</" + replace[i2] + ">)", "g");
+            var tag_to_replace_selector = tag;
+            var replacement = replace_tag;
         }
+        documentFragment.querySelectorAll(tag_to_replace_selector).forEach(tag_to_replace => replacetag(tag_to_replace, replacement))
+    });
+    tags[tag].in_tags.forEach(function(in_tag){
+        if(state != 0){
+            //firstandlastnodeon(documentFragment, tag);
 
-        html = html.replaceAll(openingtag, rep_openingtag);
-        html = html.replaceAll(closingtag, rep_closingtag);
-    }
-    return html;
+            documentFragment.querySelectorAll(in_tag).forEach(tag_to_add => addtag(tag_to_add, tag));
+        }else{
+            documentFragment.querySelectorAll(in_tag).forEach(in_tag =>in_tag.querySelectorAll(tag).forEach(tag => removetag(tag)));
+        }
+    });
 }
 
-function heading(documentFragment, on){
-    if((documentFragment.childElementCount == 0 ) && (documentFragment.childNodes.length == 1)){
-        var h1 = document.createElement("h1");
-        h1.innerHTML =  documentFragment.childNodes[0].nodeValue;
-        documentFragment.replaceChild(h1, documentFragment.childNodes[0])
-        documentFragment.appendChild(h1);
+/**
+ * if adding a tag and first node or last node is textNode
+ * @param {Node} node - part of the document to edit.
+ * @param {string} element_name - name of element from tags object (h1, b...).
+ */
+function firstandlastnodeon(node, element_name){
+    if(node.firstChild.nodeType == 3){
+        encalpsulatetag(node.firstChild, element_name);
+    }
+
+    if(node.lastChild.nodeType == 3){
+        encalpsulatetag(node.lastChild, tag);
+    }
+}
+
+/**
+ * function to replace node with element
+ * @param {Node} node node to replace
+ * @param {string} replace_element_name nodeName of the replace element
+ * @example <p></p> => <h1></h1>
+ */
+function replacetag(node, replace_element_name){
+    var replace_element = document.createElement(replace_element_name);
+    while(node.firstChild != null){
+        replace_element.appendChild(node.firstChild);
+    }
+    node.parentNode.insertBefore(replace_element, node);
+    node.remove();
+}
+
+/**
+ * function to add new element into node and transfer all child nodes to new element (as child nodes)
+ * @param {Node} node node into which shuld be note added 
+ * @param {string} add_element_name nodeName of the created element
+ * @example <div><b></b><b></b></div> => <div><p><b></b><b></b></p></div>
+ */
+function addtag(node, add_element_name){
+    var add_element = document.createElement(add_element_name);
+    while(node.firstChild != null){
+        add_element.appendChild(node.firstChild);
+    }
+/*    
+    node.childNodes.forEach(function(child){
+        add_element.appendChild(child.cloneNode();
+    });*/
+    node.appendChild(add_element);
+}
+
+/**
+ * removes node but all childnodes go to parent node
+ * @param {Node} node node which should be removed
+ * @example <div><p><b></b></p></div> => <div><b></b></div>
+ */
+function removetag(node){
+    while(node.firstChild != null){
+        insertAfter(node.firstChild, node);
+    }
+    node.remove();
+}
+
+/**
+ * crates an element after a node and moves the element into node
+ * @param {Node} node node which shuld be encapsulated
+ * @param {string} add_element_name nodeName of the created element
+ * @example <p></p> => <p><b></b></p>
+ */
+function encalpsulatetag(node, add_element_name){
+    var add_element = document.createElement(add_element_name);
+    insertAfter(add_element, node);
+    add_element.appendChild(node);
+}
+
+/**
+ *function oposit to node.insertBefore (inserts node after ref_node)
+ *@param {Node} newChild
+ *@param {Node} refChild
+ *@example <h1></h1> => <h1></h1><p></p>
+*/
+function insertAfter(newChild, refChild){
+    if(refChild.nextSibling == null){
+        refChild.parentNode.appendChild(newChild);
     }else{
-        for(i = 0; i < documentFragment.childElementCount; i++){
-            var html = documentFragment.children[i].outerHTML;
-            if(on == 1){
-                html = replacetag(html, ["p"], ["h1"], 1);
-            }else if(on == 0){
-                html = replacetag(html, ["h1"], ["p"], 1);
-            }else{
-                throw "set valid input"
-            }
-            documentFragment.children[i].outerHTML = html;
-        }
+        refChild.parentNode.insertBefore(newChild, refChild.nextSibling);
     }
-
-    return documentFragment;
-}
-
-function bolt(documentFragment, on){
-    for(i = 0; i < documentFragment.childElementCount; i++){
-        var html = documentFragment.children[i].outerHTML;
-        if(on == 1){
-            html = replacetag(html, ["p"], ["p><b"], 1);
-        }else if(on == 0){
-            html = replacetag(html, ["b"], [""], 1);
-        }else{
-            throw "set valid input"
-        }
-        documentFragment.children[i].outerHTML = html;
-    }
-
-    return documentFragment;
 }
--- adm/editor.php
+++ adm/editor.php
@@ -7,13 +7,24 @@
 		<title>SPGT-editor</title>
     </head>
     <body>
-        <button type="button" onclick="getselection('heading', 1)">make heading</button>
-        <button type="button" onclick="getselection('heading', 0)">unmake heading</button>
-        <button type="button" onclick="getselection('bolt', 1)">make bolt</button>
-        <button type="button" onclick="getselection('bolt', 0)">unmake bolt</button>
+        <button type="button" onclick="getselection('h1', 1)">make heading</button>
+        <button type="button" onclick="getselection('h1', 0)">unmake heading</button>
+        <button type="button" onclick="getselection('b', 1)">make bolt</button>
+        <button type="button" onclick="getselection('b', 0)">unmake bolt</button>
         <div class="contenteditable" contenteditable = "true" style = "">
-            <p>This text can be </p><p>abc<b>jutjh</b> edited by the user.</p>
+            <p>The</p>
+            <h1> quick</h1>
+            <p> brown </p>
+            <p>fox <b>jumps</b> over</p>
+            <p>the <b>lazy</b> dog</p>
+            <p>The <b> quick </b> brown </p>
+            <p>fox <b>jumps</b> over</p>
+            <p>the </p>
+            <h1>lazy</h1>
+            <p> dog</p>
         </div>
+        <textarea id=>
+        </textarea>
     </body>
 </html>
 <!--css should be done differently-->
