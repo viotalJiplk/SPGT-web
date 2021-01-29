372f1afd0a59e80299be6fe35e5cc07c10bdd6d9
Vojtech Varecha <viotal.varecha@outlook.com>
2021-01-29 17:12:47 +0100

    added "/" into sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,32 @@
+const cache = {
+    "version": "1.0",
+    "cache": [
+        "/js/static/pwa_cache.json",
+        "/index.php",
+        "/",
+        "/articles.php",
+        "/zasedani.php",
+        "/js/ajax.js",
+        "/js/fillzapisy.js",
+        "/js/getzapis.js",
+        "/favicon.svg",
+        "/images/title_background.jpg",
+        "/css/main_page.css"
+    ]
+}  
+//all css file should be added
+
+function caching(in_json){
+    // Open a cache of resources.
+    caches.open("spgt-v1").then(cache => {
+        return cache.addAll(in_json.cache);
+    });
+}
+ 
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        // Open a cache of resources.
-        caches.open("spgt-v1").then(cache => {
-            return cache.addAll([
-                "/index.php",
-                "/articles.php",
-                "/zasedani.php",
-                "/js/ajax.js",
-                "/js/fillzapisy.js",
-                "/js/getzapis.js"
-            ]);//here should be added all css files
-        })
-    );
-});
+    event.waitUntil(caching(cache));
+    });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
