d134b2b20719eda53c3cdb8bfda924e3fdee8534
Vojtech Varecha <viotal.varecha@outlook.com>
2021-03-15 12:07:54 +0100

    jajks wrong file

--- js/fillzapisy.js
+++ js/fillzapisy.js
@@ -32,16 +32,6 @@ function foreachzapis(record){
 //     "nrecords": 100
 // }
 
-function push_zapis(json){
-    const obj = document.getElementById(json.id).getElementsByClassName("zasedaniCont")[0];
-    const hlasovani = json.hlasovani;
-    const dochazkaZ = json.dochazkaZ;
-    const dochazkaD = json.dochazkaD;
-    const program = json.program;
-    const materialy = json.materialy;
-    const zasedaniCont = "<div class=\"program\"><h4>Program Zasedání</h4><div>" + program + "</div></div><div class=\"downloads\"><h4>Ke stažení</h4><button class=\"zapis\">Stáhnout zápis</button><div class=\"materialy\"><h5>Materiály</h5><div>" + materialy +"</div></div></div><div class=\"statistiky\"><div class=\"dochazka\"><h4>Docházka</h4><p>Zástupci tříd: " + dochazkaZ + "</p><p>Dobrovolníci: " + dochazkaD +"</p><div class=\"hlasovani\"><h4>Hlasování</h4>" + hlasovani + "</div></div>";
-    obj.innerHTML = zasedaniCont;
-}
 
 let datum = new Date();
 let payload = {
