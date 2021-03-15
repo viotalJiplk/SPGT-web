d65b0db188f0dfa7dcf7b19062a77ebf87783dc8
Vojtech Varecha <viotal.varecha@outlook.com>
2021-03-15 12:13:30 +0100

    moved foreachzapis so template could be diferentiated for normal user and adm

--- zasedani.php
+++ zasedani.php
@@ -16,4 +16,20 @@
 <script src="js/fillzapisy.js"></script>
 <script src="js/getzapis.js"></script>
 <script>
+	/**
+ 	* function to add records placeholder in site
+ 	* @param {object} record object with id of record from server and date of the record
+ 	*/
+	 function foreachzapis(record){
+    	console.log(record);
+    	id = record.id;
+    	time = record.time;
+
+    	const rec = document.createElement("div");
+    	rec.setAttribute("class", "zasedani");
+    	rec.setAttribute("id", id);
+    	const result = "<input id=\"checkbox" + id + "\" class=\"checkbox\" type=\"checkbox\" onclick=\"fill("+ id +")\"><label for=\"checkbox"+ id +"\"><div class=\"zasedaniTopWrap\"><div class=\"zasedaniTop\"><span>Zasedání "+ time +"</span><div class=\"dropdownButtonWrap\"><div class=\"dropdownButton\"></div></div></div></div></label><div class=\"zasedaniCont\"></div><!--dyn from callbackf(text)--><!--dont add anything betwen <div></div>-->"
+    	rec.innerHTML = result;
+    	root.appendChild(rec);
+	}
 </script>
